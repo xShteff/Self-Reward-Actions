6b4152e71e8d2969dbcfe61fc094005b3a8e60cf
Alin Stefan Olar <alin.stefan.olaru@lego.com>
2019-10-18 13:42:18 +0200

    Initial commit, but this  time it actually does something

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-# print the environment
-echo "-----------------------------------------------------------"
-echo "Printing environment variables"
-echo "-----------------------------------------------------------"
-
-echo $GITHUB_ACTOR
-
-/usr/bin/env
 
 if [ -n "$GITHUB_EVENT_PATH" ];
 then
     EVENT_PATH=$GITHUB_EVENT_PATH
 
-elif [ -f ./sample_push_event.json ];
+elif [ -f ./sample_event.json ];
 then
     EVENT_PATH='./sample_event.json'
 else
@@ -21,12 +13,12 @@ else
     exit 1
 fi
 
-if [ $GITHUB_ACTOR == $(jq .pull_request.merged_by.login < $EVENT_PATH) ]
-then
-    echo "YEAH";
+MERGER=$(jq .pull_request.merged_by.login < $EVENT_PATH)
+CREATOR=$(jq .pull_request.user.login < $EVENT_PATH)
+
+if [ $MERGER = $CREATOR ]; then
+    echo "YOU MERGED YOUR OWN PR! SHAME!";
 else
     echo "NO!"
 fi
 
-
-jq .pull_request.merged_by.login < $EVENT_PATH
