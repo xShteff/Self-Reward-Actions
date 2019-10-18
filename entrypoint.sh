8ef5a8e288e971696d3483e062be4689f1b719e4
Alin Stefan Olar <alin.stefan.olaru@lego.com>
2019-10-18 12:46:00 +0200

    Changed sample data to include relevant things

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,39 +9,24 @@ echo $GITHUB_ACTOR
 
 /usr/bin/env
 
-#
-# check for the environment variable GITHUB_EVENT_PATH
-# if this variable is not zero length, then the entrypoint script is being
-# run as a GitHub Action.
-#
-# GITHUB_EVENT_PATH contiains the path to the JSON  data
-# for the event that triggered the WorkFlow
-#
-# set EVENT_DATA_PATH = $GITHUB_EVENT_PATH
-#
 if [ -n "$GITHUB_EVENT_PATH" ];
 then
     EVENT_PATH=$GITHUB_EVENT_PATH
-#
-# If the entrypoint script is not being run as an Action, check for
-# a local file with sample data for testing.
-#
-# set EVENT_DATA_PATH = the local file with sample data
-#
+
 elif [ -f ./sample_push_event.json ];
 then
-    EVENT_PATH='./sample_push_event.json'
-
-#
-# If no file is available for processing, exit with an error
-#
+    EVENT_PATH='./sample_event.json'
 else
     echo "No JSON data to process. :("
     exit 1
 fi
 
-echo "-----------------------------------------------------------"
-echo "Printing $EVENT_PATH"
-echo "-----------------------------------------------------------"
+if [ $GITHUB_ACTOR == $(jq .pull_request.merged_by.login < $EVENT_PATH) ]
+then
+    echo "YEAH";
+else
+    echo "NO!"
+fi
 
-jq . < $EVENT_PATH
+
+jq .pull_request.merged_by.login < $EVENT_PATH
