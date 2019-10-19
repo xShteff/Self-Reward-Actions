cf83fd948c1b808ab4fe62b1969aa34e2369d43c
Alin Stefan Olaru <alinstefanolaru@gmail.com>
2019-10-19 17:06:38 +0200

    Restructured code?

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,15 @@
 FROM mhart/alpine-node:10
 
+RUN apk add --no-cache \
+        bash \
+        jq && \
+        which bash && \
+        which jq
+
+COPY ./src /action
+
+ENTRYPOINT ["/action/entrypoint.sh"]
+
 LABEL "maintainer"="alstol"
 LABEL "repository"="https://github.com/managedkaos/print-env"
 LABEL "com.github.actions.name"="GitHub self-medal"
@@ -7,12 +17,3 @@ LABEL "com.github.actions.description"="Posts a meme in case you merge your own 
 LABEL "com.github.actions.icon"="briefcase"
 LABEL "com.github.actions.color"="purple"
 
-RUN apk add --no-cache \
-        bash \
-        jq && \
-        which bash && \
-        which jq
-
-COPY . /usr/local/bin/
-
-ENTRYPOINT ["entrypoint.sh"]
---
+++ src/index.js
@@ -0,0 +1,52 @@
+const axios = require("axios");
+
+const NEUTRAL_ERROR_CODE = process.env.GITHUB_WORKFLOW ? 78 : 0;
+const githubEventPath = process.env.GITHUB_EVENT_PATH || "";
+const githubEvent = githubEventPath ? require(githubEventPath) : "";
+const githubRepo = process.env.GITHUB_REPOSITORY || "";
+const githubRepoUri = `https://api.github.com/repos/${githubRepo}`;
+const githubPrCommentsUri = `${githubRepoUri}/issues/${githubEvent.number}/comments`;
+const githubApiVersion = "v3";
+const githubToken = process.env.GITHUB_TOKEN || "";
+const githubAcceptHeader = `application/vnd.github.${githubApiVersion}+json; application/vnd.github.antiope-preview+json`;
+const githubAuthHeader = `token ${githubToken}`;
+const githubApiHeaders = {
+  Accept: githubAcceptHeader,
+  Authorization: githubAuthHeader
+};
+
+/**
+ * @return {Promise} Promise representing the HTTP POST of a comment.
+ */
+function postComment() {
+  console.log("Posting image...");
+  return axios.post(
+    githubPrCommentsUri,
+    { body: `Hello @alstol. Please don't merge your own PR!` },
+    {
+      headers: githubApiHeaders
+    }
+  );
+}
+
+if (
+  !githubEvent ||
+  (githubEvent.action !== "closed")
+) {
+  console.log(
+    `GitHub event payload not found or Pull Request event does not have desired action. Action was ${githubEvent.action}.`
+  );
+  process.exit(NEUTRAL_ERROR_CODE);
+}
+
+postComment()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.log(error);
+    process.exit(1);
+  });
+
+setTimeout(() => {
+  console.log("Reached maximum timeout.");
+  process.exit(1);
+}, 300000);
